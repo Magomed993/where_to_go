302905c0ec5dd07b8a419ce2b89baf7bb73ceb6b
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-12-04 16:15:22 +0300

    Add post model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField('Наименование', max_length=200)
+    text = models.TextField('Текст')
+
+    def __str__(self):
+      return self.title
