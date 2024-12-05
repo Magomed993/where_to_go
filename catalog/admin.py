25666fdf65b04cc52baea95d0d8293918a58ccbb
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-12-05 20:08:18 +0300

    Add model for images

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Place, Image
 
-admin.site.register(Post)
+
+admin.site.register(Place)
+admin.site.register(Image)
--- catalog/models.py
+++ catalog/models.py
@@ -1,9 +1,25 @@
+from modulefinder import Module
+
 from django.db import models
 
 
-class Post(models.Model):
+class Place(models.Model):
     title = models.CharField('Наименование', max_length=200)
-    text = models.TextField('Текст')
+    short_description = models.TextField('Короткое описание')
+    long_description = models.TextField('Длинное описание')
+    lng = models.FloatField('Долгота')
+    lat = models.FloatField('Широта')
 
     def __str__(self):
       return self.title
+
+
+class Image(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    image = models.ImageField('Фото')
+
+    def __str__(self):
+      return f'{self.id} - {self.place.title}'
+
+    class Meta:
+      ordering = ['-id']
