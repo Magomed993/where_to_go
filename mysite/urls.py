1dc5156e6c73f3c6e0193c1d25faf9f3496b10da
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-12-09 15:06:50 +0300

    Move data from template to view

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,7 +20,8 @@ from catalog import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index)
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
