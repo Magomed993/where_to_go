68a225588dcd1258498b42bcd8b2bdc981e85a7d
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-12-11 15:46:56 +0300

    Added preview

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
-    fields = ['image', 'order']
+    fields = ['image', 'get_preview', 'order']
     extra = 1
+    readonly_fields = ['get_preview']
 
+    def get_preview(self, obj):
+        return format_html('<img src="{}" style="max-width: 200px; max-height: 200px">', obj.image.url)
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
@@ -18,4 +23,3 @@ class PlaceAdmin(admin.ModelAdmin):
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
   pass
-# admin.site.register(Image)
