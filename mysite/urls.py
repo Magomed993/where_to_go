a1b205471831ea4685160b937cc312103d158aa3
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-12-24 20:04:57 +0300

    Uploaded JSON

--- mysite/urls.py
+++ mysite/urls.py
@@ -25,4 +25,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('places/<int:place_id>/', views.places, name='places')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- places/views.py
+++ places/views.py
@@ -1,5 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Place
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from places.models import Place, Image
+
 
 
 def index(request):
@@ -23,3 +27,18 @@ def index(request):
         "features": features
     }
     return render(request, 'index.html', context)
+
+
+def places(request, place_id):
+    place = get_object_or_404(Place, pk=place_id)
+    details_json = {
+        "title": place.title,
+        "imgs": [image.img.url for image in place.images.all()],
+        "description_short": place.description_short,
+        "description_long":  place.description_long,
+        "coordinates": {
+            "lng": place.lng,
+            "lat": place.lat
+        }
+    }
+    return JsonResponse(details_json, json_dumps_params = {"ensure_ascii": False, 'indent': 4})
