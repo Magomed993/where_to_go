7f708fc1fc928e773dbbd03065671c57a7397ef2
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-12-25 13:04:13 +0300

    Installed inlines

--- places/admin.py
+++ places/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from places.models import Place, Image
 
 
-admin.site.register(Place)
-admin.site.register(Image)
+class PlaceInline(admin.TabularInline):
+    model = Image
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [
+        PlaceInline,
+    ]
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
