6d527387301840c128155fa172e87cc38ae707fc
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-12-26 07:57:17 +0300

    Connected a text editor

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,12 +18,13 @@ Including another URLconf
 from places import views
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('places/<int:place_id>/', views.places, name='places')
+    path('places/<int:place_id>/', views.places, name='places'),
+    path('tinymce/', include('tinymce.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
