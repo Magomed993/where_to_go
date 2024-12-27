334d72e33925a26a3bfb1f40fd6951ea927de4bc
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-12-27 16:15:01 +0300

    Added by verbose_name

--- places/models.py
+++ places/models.py
@@ -9,23 +9,24 @@ class Place(models.Model):
     lng = models.FloatField(verbose_name='Долгота')
     lat = models.FloatField(verbose_name='Широта')
 
-    def __str__(self):
-        return self.title
-
     class Meta:
         verbose_name = 'место'
         verbose_name_plural = 'места'
 
+    def __str__(self):
+        return self.title
+
 
 class Image(models.Model):
-    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images')
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name='Место', related_name='images')
     img = models.ImageField(verbose_name='Картинки')
     num = models.IntegerField(verbose_name='Позиция', default=0, db_index=True)
-
-    def __str__(self):
-        return f'{self.id} {self.place.title}'
 
     class Meta:
         ordering = ['num']
         verbose_name = 'картинка'
         verbose_name_plural = 'картинки'
+
+    def __str__(self):
+        return f'{self.id} {self.place.title}'
+
